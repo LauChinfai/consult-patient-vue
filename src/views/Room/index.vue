8b58166e9a1edb07e27751cf48ea668da1d0eb10
Lau130465 <Lau130465@qq.com>
2024-05-27 13:07:55 +0800

    TODO133

--- src/views/Room/index.vue
+++ src/views/Room/index.vue
@@ -1,1 +1,26 @@
-<template>去你妈的</template>
+<script setup lang="ts">
+import roomStatus from './components/roomStatus.vue'
+import roomAction from './components/roomAction.vue'
+import roomMessage from './components/roomMessage.vue'
+</script>
+<template>
+  <div class="room-page">
+    <cp-nav-bar title="问诊室"></cp-nav-bar>
+    <room-status></room-status>
+    <room-message></room-message>
+    <roomAction></roomAction>
+  </div>
+</template>
+<style scoped lang="scss">
+.room-page {
+  padding-top: 90px;
+  padding-bottom: 60px;
+  min-height: 100vh;
+  box-sizing: border-box;
+  background-color: var(--cp-bg);
+  .van-pull-refresh {
+    width: 100%;
+    min-height: calc(100vh - 150px);
+  }
+}
+</style>
